b8deb3a4670d4d49effb309ca3981113208930ea
Thắng Bùi <thang27302@gmail.com>
2025-03-15 09:51:56 +0700

    add loading

--- src/app/tainguyen/blog/[blogId]/loading.tsx
+++ src/app/tainguyen/blog/[blogId]/loading.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function loading() {
-  return <div>loading</div>;
+  return (
+    <section className="flex items-center justify-center w-full h-full">
+      <div className="w-20 h-20 border-8 border-t-[#1AD598] rounded-full animate-spin"></div>
+    </section>
+  );
 }
 
 export default loading;
